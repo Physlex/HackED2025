74e18a706e4f050ca76aef3c371ce7f4809eb960
Alex <al.willet02@gmail.com>
2025-02-16 16:05:04 -0700

    fixed ps5 issue

--- websocket/core/controller/test.py
+++ websocket/core/controller/test.py
@@ -3,6 +3,11 @@ from pydualsense import pydualsense
 from dualsense import cross_press
 
 ds = pydualsense()
+print("Dualsense intialized")
 ds.init()
 ds.cross_pressed += cross_press
+
+while not ds.state.R1:
+    pass
+
 ds.close() # Close the connection after processing

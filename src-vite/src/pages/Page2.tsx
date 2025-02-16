06bcafb5774a3e2cfcaf59dcaf7006027f45c741
tegen-ux <117418989+tegen-ux@users.noreply.github.com>
2025-02-16 14:17:29 -0700

    Added a new navigation for our cool website, and we can expand if needed

--- src-vite/src/pages/Page2.tsx
+++ src-vite/src/pages/Page2.tsx
@@ -1,11 +1,55 @@
 import { Link } from "react-router-dom"
+import * as React from "react";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { Button } from "@mui/material";
+
+
+export function BasicMenu() {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div>
+            <Button
+                id="basic-button"
+                aria-controls={open ? "basic-menu" : undefined}
+                area-haspopup="true"
+                area-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+            >
+                Dashboard
+            </Button>
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                }}
+            >
+                <MenuItem onClick={handleClose}><Link to="/"> Home</Link></MenuItem>
+                <MenuItem onClick={handleClose}> <Link to="/ConstantRunningGraphOverTime">Cool Graph </Link> </MenuItem>
+            </Menu>
+
+        </div>
+    );
+}
+
+
 
 export default function Page2() {
     return (
         <div className="Page2">
             <nav>
-                <Link to="/">Home </Link>
-                <Link to="/ConstantRunningGraphOverTime">ConstantRunningGraphOverTime</Link>      
+                <BasicMenu/>
             </nav>
             <h1>
                 Page 2
